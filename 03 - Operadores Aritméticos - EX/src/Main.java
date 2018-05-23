a1eacaaf72514c66a9d10c806633865b47bcc2db
Márcio Lôbo <marcioloboti@gmail.com>
2018-05-23 18:54:48 -0300

    Exercicio sobre operadores aritméticos

---
+++ 03 - Operadores Aritméticos - EX/src/Main.java
@@ -0,0 +1,23 @@
+
+public class Main {
+
+    /*
+    EXERCEÍCIOS SOBRE OPERADORES ARITIMÉTICOS
+    
+    01 - Crie algumas variáveis do tipo double e faça o calculo da media, mediana e moda dos valores
+     */
+    public static void main(String[] args) {
+
+        //Declarando as variáveis
+    
+    double x =9, y=14, z=34;
+    
+    //média
+        
+        System.out.println("A média de x,y,z é : "+ ((x+y+z)/3));
+        System.out.println("A Mediana da sequencia \\{x,x,y,z,z\\}é : " + y);
+        System.out.println("A Moda da sequencia \\{x,x,y,z\\}é : " + x);
+        
+    }
+
+}
