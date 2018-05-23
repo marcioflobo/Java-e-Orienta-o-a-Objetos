c14fcf18c60628006d5a46bebfea74dcbf41623e
Márcio Lôbo <marcioloboti@gmail.com>
2018-05-23 20:29:37 -0300

    Entrada de Dados Com Scanner

---
+++ 08 - Entrada de Dados Com Scanner/src/Main.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println(" Informe o valor de X");
+        double x = in.nextDouble();
+        System.out.println("Informe o valor de Y");
+        double y = in.nextDouble();
+        System.out.println("A Soma de X = " + x + "e Y = " + y + " é igural a : " + (x + y));
+
+    }
+
+}
