085c2d3689cc60613eba6f81d5fdb4c420937b5d
Márcio Lôbo <marcioloboti@gmail.com>
2018-05-23 20:22:12 -0300

    Funções do  objeto String

---
+++ 07 - Strings/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String s = "I LOVE JAVAS";
+        System.out.println("s.replace(\"S\", \"X\") -->  " + s.replace("S", "X"));
+        System.out.println("s.toLowerCase() --> " + s.toLowerCase());
+        System.out.println(" s.toUpperCase() --> " + s.toUpperCase());
+        System.out.println("s.trim() --> " + s.trim());
+        System.out.println(" s.charAt(7) --> " + s.charAt(7));
+        System.out.println(" s.intern() -->" + s.intern());
+    }
+
+}
