c87f5cda337326e4b933e1e6d29c50b7353d7c05
Márcio Lôbo <marcioloboti@gmail.com>
2018-05-23 18:37:03 -0300

    Variáveis

---
+++ 02 - Variáveis/src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+
+  
+    public static void main(String[] args) {
+        
+        int x = 10;
+        int y =70;
+        System.out.println("O valor de X é : " + x);
+        System.out.println("O valor de y é : " + y);
+        System.out.println("A Soma  é : " + (x+y));
+        
+        String a = "Hello";
+        String b = "World";
+        System.out.println("Valor de A: " + a );
+        System.out.println("Valor de B: " + b );
+        
+        System.out.println("A Soma de A+ B é : " + a +" "+ b);
+                
+    }
+    
+}
