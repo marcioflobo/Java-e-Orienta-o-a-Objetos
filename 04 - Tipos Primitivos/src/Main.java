d07b8a5db828b799d39a4b0e0eba2cc05bd8aa12
Márcio Lôbo <marcioloboti@gmail.com>
2018-05-23 19:15:03 -0300

    Tipos Primitivos

---
+++ 04 - Tipos Primitivos/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+        byte b = 127;
+        short s = 32767;
+        int i = 2146999999;
+        long l =8999999999999999999l;
+        float f =299999999999999999999999999999999999999f;
+        
+        System.out.println("");
+    }
+}
