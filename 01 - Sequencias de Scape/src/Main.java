4ae84fe891b5f09009080af4df8d781be203afdd
Márcio Lôbo <marcioloboti@gmail.com>
2018-05-23 18:20:11 -0300

    Caracteres de Scape

---
+++ 01 - Sequencias de Scape/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Imprime uma barra
+        System.out.println("\\");
+        
+        // Imprime áspas
+        System.out.println("\"");
+        
+        //Quebra \n Quebra una linha
+        System.out.println("Hello\nWorld");
+        
+        // insere uma tabulação \t 
+        System.out.println("Hello\tWorld");
+    }
+    
+}
