3a720fcc5c3bda053a33968f29d67c5c6686c72d
Márcio Lôbo <marcioloboti@gmail.com>
2018-05-23 18:44:19 -0300

    Operadores Aritiméticos

---
+++ 03 - Operadores Aritméticos/src/Main.java
@@ -0,0 +1,28 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        double x = 30, y =51;
+        
+        // Soma:  "+"
+        System.out.println("Operações envolvendo x & y");
+        System.out.println("-----------------------------------");
+        System.out.println("A Soma de x+y é : " + (x+y));
+        
+        System.out.println("-----------------------------------");
+        System.out.println("A Subtração é : "+ (x-y));
+        
+        System.out.println("-----------------------------------");
+        System.out.println("A Divisão é :"+(x/y));
+        
+        System.out.println("-----------------------------------");
+        System.out.println("A Multiplicação é : "+(x*y));
+        
+        System.out.println("-----------------------------------");
+        System.out.println("O Resto da divisão de x  é : "+ (x%2));
+        System.out.println("O Resto da divisão de y  é : "+ (y%2));
+        
+    }
+    
+}
