61e407a70c1c76d27810288b660cfd015b6bdffb
Márcio Lôbo <marcioloboti@gmail.com>
2018-05-24 10:12:24 -0300

    Estrutura Do While

---
+++ 15 - Estrutura Do While/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+ 
+    public static void main(String[] args) {
+      
+        int x = 0;
+            
+        do{
+            System.out.println("X = " + x);
+            x++;
+        }while(x<10);
+    }
+    
+}
