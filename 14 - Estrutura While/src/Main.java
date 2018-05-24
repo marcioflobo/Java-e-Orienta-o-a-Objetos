7f715c08508bcb30fd52727b4018f060d3eed92f
Márcio Lôbo <marcioloboti@gmail.com>
2018-05-24 10:09:58 -0300

    Estrutura de Controle While

---
+++ 14 - Estrutura While/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        int x = 0;
+        while( x<5){
+            System.out.println("X =  " + x);
+            x++;
+        }
+    }
+    
+}
