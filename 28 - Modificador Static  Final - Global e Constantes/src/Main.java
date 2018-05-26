88e4f84697d8edb1471d6df9b43d5bf086e0ae2c
Márcio Lôbo <marcioloboti@gmail.com>
2018-05-26 08:42:49 -0300

    Modificador Static Final - Golbal e Constantes

---
+++ 28 - Modificador Static  Final - Global e Constantes/src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+    
+public static final double PI = 3.14;
+     
+public static double area(double r){
+         
+         double a = (r * (PI*PI));
+         return a;
+         
+}
+
+   
+    public static void main(String[] args) {
+          
+         System.out.println("Area :" + area(5));
+       
+    }
+    
+}
